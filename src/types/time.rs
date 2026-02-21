43bd2012d1875eeb3ac43f95cde87fedba3140ce
Xanthorrhizol <xanthorrhizol@proton.me>
2026-02-21 15:50:20 +0900

    R: Time::now method redefined

--- src/types/time.rs
+++ src/types/time.rs
@@ -1,16 +1,12 @@
 use crate::Error;
 use chrono::{DateTime, LocalResult};
-use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug, Clone)]
 pub struct Time(DateTime<chrono_tz::Tz>);
 
 impl Time {
     pub fn now() -> Self {
-        let now = SystemTime::now();
-        let millis = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
-        let naive_dt = chrono::NaiveDateTime::from_timestamp_millis(millis as i64).unwrap();
-        Self(naive_dt.and_local_timezone(chrono_tz::Asia::Seoul).unwrap())
+        Self(chrono::Utc::now().with_timezone(&chrono_tz::Asia::Seoul))
     }
     pub fn parse(s: &str, fmt: &str) -> Result<Self, Error> {
         let naive_dt = chrono::NaiveDateTime::parse_from_str(s, fmt)?;
